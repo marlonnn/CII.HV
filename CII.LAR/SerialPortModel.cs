93e268e3060764c6469b0368e5027ae4f5991c9b
Vincent <zhongw_1988@126.com>
2017-12-12 08:47:22 +0800

    增加激光和电机串口

--- CII.LAR/SerialPortModel.cs
+++ CII.LAR/SerialPortModel.cs
@@ -13,31 +13,21 @@ namespace CII.LAR
         private SerialPort laserSerialPort = new SerialPort();
         private SerialPort motorSerialPort = new SerialPort();
 
-        public event SerialPortEventHandler laserComReceiveDataEvent = null;
         public event SerialPortEventHandler laserComOpenEvent = null;
         public event SerialPortEventHandler laserComCloseEvent = null;
 
-        public event SerialPortEventHandler motorComReceiveDataEvent = null;
         public event SerialPortEventHandler motorComOpenEvent = null;
         public event SerialPortEventHandler motorComCloseEvent = null;
 
         private Object thisLock = new Object();
 
-        /// <summary>
-        /// When serial received data, will call this method
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void LaserDataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             if (laserSerialPort.BytesToRead <= 0)
             {
                 return;
             }
-            //Thread Safety explain in MSDN:
-            // Any public static (Shared in Visual Basic) members of this type are thread safe. 
-            // Any instance members are not guaranteed to be thread safe.
-            // So, we need to synchronize I/O
+
             lock (thisLock)
             {
                 int len = laserSerialPort.BytesToRead;
@@ -46,15 +36,32 @@ namespace CII.LAR
                 {
                     laserSerialPort.Read(data, 0, len);
                 }
-                catch (System.Exception)
+                catch (System.Exception ex)
                 {
-                    //catch read exception
+                    LogHelper.GetLogger<SerialPortModel>().Error(string.Format("激光器串口接收数据异常： {0}", ex.StackTrace));
                 }
-                SerialPortEventArgs args = new SerialPortEventArgs();
-                args.receivedBytes = data;
-                if (laserComReceiveDataEvent != null)
+            }
+        }
+
+
+        private void MotorDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (motorSerialPort.BytesToRead <= 0)
+            {
+                return;
+            }
+
+            lock (thisLock)
+            {
+                int len = motorSerialPort.BytesToRead;
+                Byte[] data = new Byte[len];
+                try
                 {
-                    laserComReceiveDataEvent.Invoke(this, args);
+                    motorSerialPort.Read(data, 0, len);
+                }
+                catch (System.Exception ex)
+                {
+                    LogHelper.GetLogger<SerialPortModel>().Error(string.Format("电机串口接收数据异常： {0}", ex.StackTrace));
                 }
             }
         }
@@ -75,15 +82,35 @@ namespace CII.LAR
             {
                 laserSerialPort.Write(bytes, 0, bytes.Length);
             }
-            catch (System.Exception)
+            catch (System.Exception ex)
             {
-                return false;   //write failed
+                LogHelper.GetLogger<SerialPortModel>().Error(string.Format("激光器串口发送数据异常： {0}", ex.StackTrace));
+                return false; 
             }
-            return true;        //write successfully
+            return true;
+        }
+
+        public bool MotorSendData(Byte[] bytes)
+        {
+            if (!motorSerialPort.IsOpen)
+            {
+                return false;
+            }
+
+            try
+            {
+                motorSerialPort.Write(bytes, 0, bytes.Length);
+            }
+            catch (System.Exception ex)
+            {
+                LogHelper.GetLogger<SerialPortModel>().Error(string.Format("电机串口发送数据异常： {0}", ex.StackTrace));
+                return false;
+            }
+            return true; 
         }
 
         /// <summary>
-        /// Open Serial port
+        /// Open Laser Serial port
         /// </summary>
         /// <param name="portName"></param>
         /// <param name="baudRate"></param>
@@ -97,7 +124,7 @@ namespace CII.LAR
         {
             if (laserSerialPort.IsOpen)
             {
-                Close();
+                CloseLaserSerialThread();
             }
             laserSerialPort.PortName = portName;
             laserSerialPort.BaudRate = Convert.ToInt32(baudRate);
@@ -142,42 +169,85 @@ namespace CII.LAR
 
         }
 
-        /**
-         *  Take care to avoid deadlock when calling Close on the SerialPort 
-         *  in response to a GUI event.
-         *   An app involving the UI and the SerialPort freezes up when closing the SerialPort
-         *   Deadlock can occur if Control.Invoke() is used in serial port event handlers
-         * 
-         *  The typical scenario we encounter is occasional deadlock in an app 
-         *  that has a data received handler trying to update the GUI at the 
-         *  same time the GUI thread is trying to close the SerialPort (for 
-         *  example, in response to the user clicking a Close button).
-         * 
-         *  The reason deadlock happens is that Close() waits for events to 
-         *  finish executing before it closes the port. You can address this 
-         *  problem in your apps in two ways:
-         * 
-         *  (1)In your event handlers, replace every Control.Invoke call with 
-         *  Control.BeginInvoke, which executes asynchronously and avoids 
-         *  the deadlock condition. This is commonly used for deadlock avoidance 
-         *  when working with GUIs.
-         *  
-         *  (2)Call serialPort.Close() on a separate thread. You may prefer this
-         *  because this is less invasive than updating your Invoke calls.
-         */
+        /// <summary>
+        /// Open motor serial port
+        /// </summary>
+        /// <param name="portName"></param>
+        /// <param name="baudRate"></param>
+        /// <param name="dataBits"></param>
+        /// <param name="stopBits"></param>
+        /// <param name="parity"></param>
+        /// <param name="handshake"></param>
+        public void MotorSerialPortOpen(string portName, String baudRate,
+            string dataBits, string stopBits, string parity,
+            string handshake)
+        {
+            if (motorSerialPort.IsOpen)
+            {
+                CloseMotorSerialThread();
+            }
+            motorSerialPort.PortName = portName;
+            motorSerialPort.BaudRate = Convert.ToInt32(baudRate);
+            motorSerialPort.DataBits = Convert.ToInt16(dataBits);
+
+            /**
+             *  If the Handshake property is set to None the DTR and RTS pins 
+             *  are then freed up for the common use of Power, the PC on which
+             *  this is being typed gives +10.99 volts on the DTR pin & +10.99
+             *  volts again on the RTS pin if set to true. If set to false 
+             *  it gives -9.95 volts on the DTR, -9.94 volts on the RTS. 
+             *  These values are between +3 to +25 and -3 to -25 volts this 
+             *  give a dead zone to allow for noise immunity.
+             *  http://www.codeproject.com/Articles/678025/Serial-Comms-in-Csharp-for-Beginners
+             */
+            if (handshake == "None")
+            {
+                //Never delete this property
+                motorSerialPort.RtsEnable = true;
+                motorSerialPort.DtrEnable = true;
+            }
+
+            SerialPortEventArgs args = new SerialPortEventArgs();
+            try
+            {
+                motorSerialPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), stopBits);
+                motorSerialPort.Parity = (Parity)Enum.Parse(typeof(Parity), parity);
+                motorSerialPort.Handshake = (Handshake)Enum.Parse(typeof(Handshake), handshake);
+                motorSerialPort.WriteTimeout = 1000; /*Write time out*/
+                motorSerialPort.Open();
+                motorSerialPort.DataReceived += new SerialDataReceivedEventHandler(MotorDataReceived);
+                args.isOpend = true;
+            }
+            catch (System.Exception)
+            {
+                args.isOpend = false;
+            }
+            if (laserComOpenEvent != null)
+            {
+                motorComOpenEvent.Invoke(this, args);
+            }
+
+        }
+
         /// <summary>
         /// Close serial port
         /// </summary>
-        public void Close()
+        public void CloseLaserSerialThread()
         {
-            Thread closeThread = new Thread(new ThreadStart(CloseSpThread));
+            Thread closeThread = new Thread(new ThreadStart(CloseLaserSpThread));
+            closeThread.Start();
+        }
+
+        public void CloseMotorSerialThread()
+        {
+            Thread closeThread = new Thread(new ThreadStart(CloseMotorSpThread));
             closeThread.Start();
         }
 
         /// <summary>
         /// Close serial port thread
         /// </summary>
-        private void CloseSpThread()
+        private void CloseLaserSpThread()
         {
             SerialPortEventArgs args = new SerialPortEventArgs();
             args.isOpend = false;
@@ -195,6 +265,25 @@ namespace CII.LAR
                 laserComCloseEvent.Invoke(this, args);
             }
 
+        }
+
+        private void CloseMotorSpThread()
+        {
+            SerialPortEventArgs args = new SerialPortEventArgs();
+            args.isOpend = false;
+            try
+            {
+                motorSerialPort.Close(); //close the serial port
+                motorSerialPort.DataReceived -= new SerialDataReceivedEventHandler(MotorDataReceived);
+            }
+            catch (Exception)
+            {
+                args.isOpend = true;
+            }
+            if (motorComCloseEvent != null)
+            {
+                motorComCloseEvent.Invoke(this, args);
+            }
         }
     }
 }

8b3691e4e520d8b33d175656707b76f3801d0566
Vincent <zhongw_1988@126.com>
2018-06-26 20:06:13 +0800

    MaterialTitleBar 增加绘制标题

--- CII.LAR/MaterialSkin/MaterialTitleBar.cs
+++ CII.LAR/MaterialSkin/MaterialTitleBar.cs
@@ -149,9 +149,14 @@ namespace CII.LAR.MaterialSkin
                 var iconRect = new Rectangle(8, 4, 24, 24);
                 g.DrawImage(Icon.ToBitmap(), iconRect);
             }
+            //Form title
 
+            g.DrawString(Text, SkinManager.PINGFANG_MEDIUM_16, SkinManager.ColorScheme.TextBrush,
+                new Rectangle(SkinManager.FORM_PADDING + 32, 0, Width, STATUS_BAR_HEIGHT),
+                new StringFormat { LineAlignment = StringAlignment.Center });
         }
 
+        private const int STATUS_BAR_HEIGHT = 32;
         public EventHandler MaxHandler;
         public EventHandler CloseHandler;
         public EventHandler MinHandler;

dcca019e3d4508650ffd2b293ee755bb613e4c1e
Vincent <zhongw_1988@126.com>
2018-07-05 23:20:50 +0800

    SmartRuler

--- CII.LAR/DrawTools/SmartRuler.cs
+++ CII.LAR/DrawTools/SmartRuler.cs
@@ -43,14 +43,128 @@ namespace CII.LAR.DrawTools
                 GraphicsProperties gp = Program.SysConfig.GraphicsPropertiesManager.GetPropertiesByName("Ruler");
                 using (Pen pen = new Pen(Color.FromArgb(gp.Alpha, gp.Color), gp.PenWidth))
                 {
-                    var baseMicron = (100 / Program.SysConfig.Lense.Factor) * pictureBox.DigitalMagnification;
+                    var baseMicron = BaseMajorValue * pictureBox.DigitalMagnification;
                     micronPixels = pictureBox.MicronToPixel(baseMicron) * Program.SysConfig.Lense.FineAdjustment / 100f;
-
                     PaintXAxis(g, pen, gp);
                     PaintYAxis(g, pen, gp);
                 }
             }
         }
+
+        private double baseMajorValue;
+        public double BaseMajorValue
+        {
+            get { return baseMajorValue = CalculateMajorValue() / RoundFactor; }
+            set { baseMajorValue = value; }
+        }
+        public double CalculateMajorValue()
+        {
+            double value = 0;
+            if (100 % RoundFactor != 0)
+            {
+                var temp = 100 / RoundFactor;
+                value = temp;
+            }
+            else
+            {
+                value = 100 / RoundFactor;
+            }
+            return value * RoundFactor;
+        }
+
+        public int RoundFactor
+        {
+            get { return CalculateRoundFactor(); }
+        }
+        private int CalculateRoundFactor()
+        {
+            int factor = (int)Program.SysConfig.Lense.Factor;
+            int roundFactor = 1;
+            if (factor > 0 && factor < 10)
+            {
+                roundFactor = 1;
+            }
+            else if (factor >= 10 && factor < 100)
+            {
+                roundFactor = (int)(factor / 10) * 10;
+            }
+            else if (factor >= 100 && factor <1000)
+            {
+                roundFactor = (int)(factor / 100) * 100;
+            }
+            return roundFactor;
+        }
+
+
+        private void PaintYDividePositiveAxis(Graphics g, Pen pen, GraphicsProperties gp, int index, int startX, int endX, float startY, float endY)
+        {
+            var count = micronPixels / (40 * Program.SysConfig.Lense.FineAdjustment / 100f);
+            count = GetCount((int)count);
+            if (count > 1)
+            {
+                double positiveStartNumber = (index - 1) * baseMajorValue;
+
+                double positiveEndNumber = index * baseMajorValue;
+                var v = baseMajorValue / count;
+                int j = 0;
+                for (double i = startY; i < endY; i += (micronPixels / count))
+                {
+                    if (i != startY)
+                    {
+                        //g.DrawLine(pen, (float)i, startY, (float)i, endY);
+                        g.DrawLine(pen, startX, (float)i, endX, (float)i);
+                        j++;
+                        Font font = new Font("Microsoft Sans Serif", 8.25f + gp.PenWidth / 10f);
+                        double positiveNumber = positiveStartNumber + (positiveEndNumber - positiveStartNumber) / count * j;
+
+                        string pns = positiveNumber.ToString("F2");
+                        var pSize = g.MeasureString(pns, font);
+                        if (positiveNumber > 0)
+                        {
+                            using (SolidBrush sb = new SolidBrush(Color.FromArgb(gp.Alpha, gp.Color)))
+                                g.DrawString(pns, font, sb, endX - pSize.Width, (float)(i - pSize.Height / 2));
+                        }
+                    }
+                }
+            }
+        }
+
+        private void PaintYDivideNegativeAxis(Graphics g, Pen pen, GraphicsProperties gp, int index, int startX, int endX, float startYNegative, float endY)
+        {
+            var count = micronPixels / (40 * Program.SysConfig.Lense.FineAdjustment / 100f);
+            count = GetCount((int)count);
+            if (count > 1)
+            {
+                double positiveStartNumber = (index + 1) * baseMajorValue;
+
+                double positiveEndNumber = index * baseMajorValue;
+                int j = 0;
+                for (double i = startYNegative; i < endX; i += (micronPixels / count))
+                {
+                    if (i != startYNegative)
+                    {
+                        g.DrawLine(pen, startX, (float)i, endX, (float)i);
+                        //g.DrawLine(pen, (float)startXNegative, startY, (float)startXNegative, endY);
+                        j++;
+                        Font font = new Font("Microsoft Sans Serif", 8.25f + gp.PenWidth / 10f);
+                        double positiveNumber = positiveStartNumber + (positiveEndNumber - positiveStartNumber) / count * j;
+                        //string pns = positiveNumber.ToString("F2");
+                        //var pSize = g.MeasureString(pns, font);
+
+                        var negativeNumber = -1 * positiveNumber;
+                        var nns = negativeNumber.ToString("F2");
+                        var nSize = g.MeasureString(nns, font);
+                        if (negativeNumber < 0)
+                        {
+                            using (SolidBrush sb = new SolidBrush(Color.FromArgb(gp.Alpha, gp.Color)))
+                                //g.DrawString(nns, font, sb, (float)(i - nSize.Width / 2), endY - nSize.Height);
+                                g.DrawString(nns, font, sb, endX - nSize.Width, (float)(i - nSize.Height / 2));
+                        }
+                    }
+                }
+            }
+        }
+
 
         private void PaintYAxis(Graphics g, Pen pen, GraphicsProperties gp)
         {
@@ -63,14 +177,15 @@ namespace CII.LAR.DrawTools
             //micronPixels = CentimeterToPixel(1) * Program.SysConfig.Lense.FineAdjustment / 100f;
             int count = 0;
 
-            for (double i = startYPositive; i < endYPositive; i+=micronPixels)
+            for (double i = startYPositive; i < endYPositive + micronPixels; i+=micronPixels)
             {
-                var positiveNumber = count * 100 / Program.SysConfig.Lense.Factor/* / (Program.SysConfig.Lense.Factor * pictureBox.Zoom)*/;
+                var positiveNumber = count * baseMajorValue/* / (Program.SysConfig.Lense.Factor * pictureBox.Zoom)*/;
                 //draw positive major ticks
                 g.DrawLine(pen, startX, (float)i, endX, (float)i);
-
+                if (i != startYPositive)
+                    PaintYDividePositiveAxis(g, pen, gp, count, startX, endX, (float)(i - micronPixels), (float)i);
                 //draw positive minor ticks
-                g.DrawLine(pen, startX, (float)(i + micronPixels / 2), startX - 5, (float)(i + micronPixels / 2));
+                //g.DrawLine(pen, startX, (float)(i + micronPixels / 2), startX - 5, (float)(i + micronPixels / 2));
 
                 string pns = positiveNumber.ToString("F2");
                 Font font = new Font("Microsoft Sans Serif", 8.25f + gp.PenWidth / 10f);
@@ -84,7 +199,7 @@ namespace CII.LAR.DrawTools
                 //draw negative major ticks
                 g.DrawLine(pen, startX, (float)startYNegative, endX, (float)startYNegative);
                 //draw negative minor ticks
-                g.DrawLine(pen, startX, (float)(startYNegative - micronPixels / 2), startX - 5, (float)(startYNegative - micronPixels / 2));
+                //g.DrawLine(pen, startX, (float)(startYNegative - micronPixels / 2), startX - 5, (float)(startYNegative - micronPixels / 2));
 
                 var negativeNumber = -1 * positiveNumber;
                 var nns = negativeNumber.ToString("F2");
@@ -95,6 +210,7 @@ namespace CII.LAR.DrawTools
                         g.DrawString(nns, font, sb, endX - nSize.Width, (float)(startYNegative - nSize.Height / 2));
                 }
                 startYNegative -= micronPixels;
+                PaintYDivideNegativeAxis(g, pen, gp, count, startX, endX, (float)startYNegative, (float)(startYNegative + micronPixels));
                 count++;
                 font.Dispose();
             }
@@ -102,6 +218,92 @@ namespace CII.LAR.DrawTools
             g.DrawLine(pen, pictureBox.Width / 2, 0, pictureBox.Width / 2, pictureBox.Height);
         }
 
+
+        private void PaintXDividePositiveAxis(Graphics g, Pen pen, GraphicsProperties gp, int index, double startX, double endX, float startY, float endY)
+        {
+            var count = micronPixels / (40 * Program.SysConfig.Lense.FineAdjustment / 100f);
+            count = GetCount((int)count);
+            if (count > 1)
+            {
+                double positiveStartNumber = (index - 1) * baseMajorValue;
+
+                double positiveEndNumber = index * baseMajorValue;
+                var v = baseMajorValue / count;
+                int j = 0;
+                for (double i= startX; i < endX; i+= (micronPixels / count))
+                {
+                    if (i != startX)
+                    {
+                        g.DrawLine(pen, (float)i, startY, (float)i, endY);
+                        j++;
+                        Font font = new Font("Microsoft Sans Serif", 8.25f + gp.PenWidth / 10f);
+                        double positiveNumber = positiveStartNumber + (positiveEndNumber - positiveStartNumber) / count * j;
+                        string pns = positiveNumber.ToString("F2");
+                        var pSize = g.MeasureString(pns, font);
+                        if (positiveNumber > 0)
+                        {
+                            using (SolidBrush sb = new SolidBrush(Color.FromArgb(gp.Alpha, gp.Color)))
+                                g.DrawString(pns, font, sb, (float)(i - pSize.Width / 2), endY - pSize.Height);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void PaintXDivideNegativeAxis(Graphics g, Pen pen, GraphicsProperties gp, int index, double startXNegative, double endX, float startY, float endY)
+        {
+            var count = micronPixels / (40 * Program.SysConfig.Lense.FineAdjustment / 100f);
+            count = GetCount((int)count);
+            if (count > 1)
+            {
+                double positiveStartNumber = (index + 1) * baseMajorValue;
+
+                double positiveEndNumber = index * baseMajorValue;
+                var v = baseMajorValue / count;
+                int j = 0;
+                for (double i = startXNegative; i < endX; i += (micronPixels / count))
+                {
+                    if (i != startXNegative)
+                    {
+                        g.DrawLine(pen, (float)i, startY, (float)i, endY);
+                        //g.DrawLine(pen, (float)startXNegative, startY, (float)startXNegative, endY);
+                        j++;
+                        Font font = new Font("Microsoft Sans Serif", 8.25f + gp.PenWidth / 10f);
+                        double positiveNumber = positiveStartNumber + (positiveEndNumber - positiveStartNumber) / count * j;
+                        //string pns = positiveNumber.ToString("F2");
+                        //var pSize = g.MeasureString(pns, font);
+
+                        var negativeNumber = -1 * positiveNumber;
+                        var nns = negativeNumber.ToString("F2");
+                        var nSize = g.MeasureString(nns, font);
+                        if (negativeNumber < 0)
+                        {
+                            using (SolidBrush sb = new SolidBrush(Color.FromArgb(gp.Alpha, gp.Color)))
+                                g.DrawString(nns, font, sb, (float)(i - nSize.Width / 2), endY - nSize.Height);
+                        }
+
+                        //if (positiveNumber > 0)
+                        //{
+                        //    using (SolidBrush sb = new SolidBrush(Color.FromArgb(gp.Alpha, gp.Color)))
+                        //        g.DrawString(pns, font, sb, (float)(i - pSize.Width / 2), endY - pSize.Height);
+                        //}
+                    }
+                }
+            }
+        }
+
+        private int GetCount(int count)
+        {
+            if ( baseMajorValue % count != 0)
+            {
+                if (count> 0 && count-1 > 0)
+                {
+                    count--;
+                    return GetCount(count);
+                }
+            }
+            return count;
+        }
         private void PaintXAxis(Graphics g, Pen pen, GraphicsProperties gp)
         {
             var startXPositive = pictureBox.Width / 2;
@@ -112,15 +314,16 @@ namespace CII.LAR.DrawTools
             double startXNegative = pictureBox.Width / 2;
             //micronPixels = CentimeterToPixel(1) * Program.SysConfig.Lense.FineAdjustment / 100f;
             int count = 0;
-            for (double i= startXPositive; i < endXPositive; i+= micronPixels)
+            for (double i= startXPositive; i < endXPositive + micronPixels; i+= micronPixels)
             {
 
-                var positiveNumber = count * 100 / Program.SysConfig.Lense.Factor/* / (Program.SysConfig.Lense.Factor * pictureBox.Zoom)*/;
+                var positiveNumber = count * baseMajorValue/* / (Program.SysConfig.Lense.Factor * pictureBox.Zoom)*/;
                 //draw positive major ticks
                 g.DrawLine(pen, (float)i, startY, (float)i, endY);
-
+                if (i != startXPositive)
+                    PaintXDividePositiveAxis(g, pen, gp, count, i - micronPixels, i, startY, endY);
                 //draw positive minor ticks
-                g.DrawLine(pen, (float)(i + micronPixels / 2), startY, (float)(i + micronPixels / 2), startY - 5);
+                //g.DrawLine(pen, (float)(i + micronPixels / 2), startY, (float)(i + micronPixels / 2), startY - 5);
 
                 Font font = new Font("Microsoft Sans Serif", 8.25f + gp.PenWidth / 10f);
                 string pns = positiveNumber.ToString("F2");
@@ -134,7 +337,7 @@ namespace CII.LAR.DrawTools
                 //draw negative major ticks
                 g.DrawLine(pen, (float)startXNegative, startY, (float)startXNegative, endY);
                 //draw negative minor ticks
-                g.DrawLine(pen, (float)(startXNegative - micronPixels / 2), startY, (float)(startXNegative - micronPixels / 2), startY - 5);
+                //g.DrawLine(pen, (float)(startXNegative - micronPixels / 2), startY, (float)(startXNegative - micronPixels / 2), startY - 5);
 
                 var negativeNumber = -1 * positiveNumber;
                 var nns = negativeNumber.ToString("F2");
@@ -143,8 +346,10 @@ namespace CII.LAR.DrawTools
                 {
                     using (SolidBrush sb = new SolidBrush(Color.FromArgb(gp.Alpha, gp.Color)))
                         g.DrawString(nns, font, sb, (float)(startXNegative - nSize.Width / 2), endY - nSize.Height);
-                } 
+                }
+                //if (i != startXNegative)
                 startXNegative -= micronPixels;
+                PaintXDivideNegativeAxis(g, pen, gp, count, startXNegative, startXNegative + micronPixels, startY, endY);
                 count++;
                 font.Dispose();
             }
